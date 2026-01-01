77674e680f84e6c8b59ef17897012eecc88598bf
rotsu1 <ryuotsu123@gmail.com>
2026-01-01 14:04:02 +1000

    Add SQL code for migrating talk_user table

---
+++ backend/internal/migrations/2026010104_create_talk_user.sql
@@ -0,0 +1,10 @@
+--- +up
+CREATE TABLE talk_user (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+--- +down
+DROP TABLE talk_user;
+
