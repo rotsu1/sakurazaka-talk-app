99a85af34c013aa9449e47b2f647f494640d8bde
rotsu1 <ryuotsu123@gmail.com>
2026-01-03 10:48:42 +1000

    Create plain swift project

---
+++ frontend/sakurazaka-talk-app/sakurazaka-talk-app/sakurazaka_talk_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sakurazaka_talk_appApp.swift
+//  sakurazaka-talk-app
+//
+//  Created by 乙津　龍　 on 3/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct sakurazaka_talk_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
