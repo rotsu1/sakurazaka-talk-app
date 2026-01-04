979fe76fb7321e5fc0b14381819a91c0cb6ae096
rotsu1 <ryuotsu123@gmail.com>
2026-01-04 19:17:02 +1000

    Add constant for color mappings for tag in news

--- frontend/sakurazaka-talk-app/sakurazaka-talk-app/Utils/Color.swift
+++ frontend/sakurazaka-talk-app/sakurazaka-talk-app/Utils/Color.swift
@@ -17,3 +17,21 @@ extension Color {
 }
 
 let sakuraPink = Color.rgb(red: 241, green: 157, blue: 181)
+
+let tagColor: [String: Color] = [
+    "メディア": Color.rgb(red: 198, green: 150, blue: 222),
+    "ファンクラブ": Color.rgb(red: 124, green: 206, blue: 124),
+    "リリース": Color.rgb(red: 240, green: 138, blue: 138),
+    "イベント情報": Color.rgb(red: 245, green: 225, blue: 125),
+    "グッズ": Color.rgb(red: 238, green: 192, blue: 140),
+    "その他": Color.rgb(red: 180, green: 185, blue: 185),
+]
+
+let tagTextColor: [String: Color] = [
+    "メディア": Color.white,
+    "ファンクラブ": Color.white,
+    "リリース": Color.white,
+    "イベント情報": Color(white: 0.3),
+    "グッズ": Color(white: 0.3),
+    "その他": Color.white,
+]
